95dc08cc15d769d44c7137a014f2b9fcb6e91c48
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2020-09-17 20:35:18 +0200

    Updates

---
+++ precache-manifest.fb210737c0955cd5f7253d58cb85e0e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55fc4533db38d5b513ece9f206e866d",
+    "url": "/web-excursions/index.html"
+  },
+  {
+    "revision": "3d0cf102825f4e6a1b7e",
+    "url": "/web-excursions/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "604a2ac9764d3b707906",
+    "url": "/web-excursions/static/css/main.66e4082f.chunk.css"
+  },
+  {
+    "revision": "3d0cf102825f4e6a1b7e",
+    "url": "/web-excursions/static/js/2.93bff9b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-excursions/static/js/2.93bff9b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604a2ac9764d3b707906",
+    "url": "/web-excursions/static/js/main.17750d5f.chunk.js"
+  },
+  {
+    "revision": "0ff25fc196878befc847",
+    "url": "/web-excursions/static/js/runtime-main.9a90ecd9.js"
+  },
+  {
+    "revision": "1a311e33a07496bd2ec9b22c18c826e5",
+    "url": "/web-excursions/static/media/dresden.1a311e33.jpg"
+  },
+  {
+    "revision": "102c1dc131bfd765c2ba943234d65b56",
+    "url": "/web-excursions/static/media/dresden2.102c1dc1.jpg"
+  },
+  {
+    "revision": "3fb0c9937115ddb35cfaea63325331c2",
+    "url": "/web-excursions/static/media/dresden3.3fb0c993.jpg"
+  },
+  {
+    "revision": "6a471ad2aae258fdf19efc3d18d1f02a",
+    "url": "/web-excursions/static/media/dresden4.6a471ad2.jpg"
+  }
+]);
